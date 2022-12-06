daa7ce686dfed3b68ca332bd6429b65a3740275f
kylemocode <kylemo860617@gmail.com>
2022-12-07 00:14:08 +0800

    ➕ install & integrate tailwind CSS (server components not support CSS-in-JS now)

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
-import prisma from '@/lib/prisma';
+import { Movies } from '@prisma/client';
+
+const fetchMovies = async () => {
+  const res = await fetch(`${process.env.BASE_URL}/api/movies`, {
+    next: { revalidate: 60 },
+  });
+
+  const movies: Movies = await res.json();
+  return movies;
+};
 
 export default async function Home() {
-  const movies = await prisma.movies.findMany();
+  const movies = await fetchMovies();
   console.log(movies);
-  return <div>test</div>;
+  return <div className='text-red-500'>test</div>;
 }
---
+++ pages/api/movies.ts
@@ -0,0 +1,11 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import prisma from '@/lib/prisma';
+import { Movies } from '@prisma/client';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Movies[]>
+) {
+  const movies = await prisma.movies.findMany();
+  res.status(200).send(movies);
+}
