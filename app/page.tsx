95c5ed94f72560ddc457a3b4b22c6b02a745b107
kylemocode <kylemo860617@gmail.com>
2022-12-06 23:33:41 +0800

    ✨ prisma client setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
-export default function Home() {
+import prisma from '../lib/prisma';
+
+export default async function Home() {
+  const movies = await prisma.movies.findMany();
+  console.log(movies);
   return <div>test</div>;
 }
