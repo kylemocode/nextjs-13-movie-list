78f0acfe018160d2214241657ff2da12963ba758
kylemocode <kylemo860617@gmail.com>
2022-12-07 11:56:25 +0800

    ✨ all movies API route

--- pages/api/movies.ts
+++ pages/api/movies.ts
@@ -3,7 +3,7 @@ import prisma from '@/lib/prisma';
 import { Movies } from '@prisma/client';
 
 export default async function handler(
-  req: NextApiRequest,
+  _req: NextApiRequest,
   res: NextApiResponse<Movies[]>
 ) {
   const movies = await prisma.movies.findMany();
