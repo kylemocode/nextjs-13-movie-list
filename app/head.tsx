9fa481eb6fce26b96ef0e5975d12cae9d8b1180c
kylemocode <kylemo860617@gmail.com>
2022-12-08 11:36:45 +0800

    🔍️ add description meta

--- app/head.tsx
+++ app/head.tsx
@@ -3,6 +3,11 @@ export default function Head() {
     <>
       <title>Movier</title>
       <meta content='width=device-width, initial-scale=1' name='viewport' />
+      <meta
+        key='description'
+        name='description'
+        content='Movier: A simple full-stack movie list application which built with NextJS 13 & Prisma '
+      />
       <link rel='icon' href='/favicon.ico' />
     </>
   );
